571e7f3b980b7161b2aa9264ab894ed04f5a38c9
DESKTOP-75NJ6J9\agung <agungkurniawanfaisol@gmail.com>
2024-10-17 20:54:28 +0700

    Normalisasi Data

---
+++ TipeDataVariabel/NormalisasiData.php
@@ -0,0 +1,4 @@
+select * from api_klinik.migrations;
+update api_klinik.migrations set migration ="Dimas" where id =10;
+delete from api_klinik.migrations where id = 10
+insert into api_klinik.migrations values (11, "dimas", 6)
